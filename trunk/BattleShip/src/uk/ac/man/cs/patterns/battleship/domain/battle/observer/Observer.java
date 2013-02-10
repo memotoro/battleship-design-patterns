1ffbad5a6cfd7ff6638e8649447f5b688fb47dc6
memo.toro@gmail.com <memo.toro@gmail.com@87c766a0-8b54-2023-da77-2bbd40306aba>
2013-02-10 00:10:45 +0000

    First approach of Observer-Subject

---
+++ trunk/BattleShip/src/uk/ac/man/cs/patterns/battleship/domain/battle/observer/Observer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.man.cs.patterns.battleship.domain.battle.observer;
+
+/**
+ *
+ * @author Guillermo Antonio Toro Bayona
+ */
+public interface Observer {
+
+    public void updateObserver(Subject subject);
+
+}
